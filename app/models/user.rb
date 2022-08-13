6a462717f9e7afcb5f76e9984e01e2aaf4a63dda
gozooit <gozoo.it@protonmail.com>
2022-08-13 19:02:22 +0200

    forgot to commit, project over

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 4, maximum: 12 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
   validates :email,
-            format: { with: /^(.+)@(.+)$/, message: 'Email invalid' },
+            format: { with: /\A(.+)@(.+)$\z/, message: 'Email invalid' },
             uniqueness: { case_sensitive: false },
             length: { minimum: 4, maximum: 254 }
 end
