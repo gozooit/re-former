4ae427d84c5ca0d4939e80ab3a4d55e3e7a17973
gozooit <gozoo.it@protonmail.com>
2022-08-13 16:42:45 +0200

    validates all user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 4, maximum: 12 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email,
+            format: { with: /^(.+)@(.+)$/, message: 'Email invalid' },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
 end
